0c8f29db5b07d1429302b61c6a90a43b6686433a
valkyrienyanko <sebastianbelle074@gmail.com>
2019-02-11 00:42:33 -0600

    commit

--- src/com/valkutils/modules/ItemModule.java
+++ src/com/valkutils/modules/ItemModule.java
@@ -3,10 +3,12 @@ package com.valkutils.modules;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
@@ -14,8 +16,51 @@ import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.valkutils.ValkUtils;
+import com.vinkaitems.VinkaItems;
 
 public class ItemModule {
+	public static List<Inventory> translatedHandRecipes = new ArrayList<Inventory>();
+	public static List<ItemStack> handRecipeItems = new ArrayList<ItemStack>();
+	public List<Inventory> handNavigation = new ArrayList<Inventory>();
+	
+	public void handRecipeHomeInv() {
+		Inventory inv = null;
+		final int breakEach = 45;
+		for (int i = 0; i < handRecipeItems.size(); i++) {
+			if (i % breakEach == 0) {
+				inv = Bukkit.createInventory(null, 9 * 6, "Hand Recipe Navigation");
+				inv.setItem(45, ItemModule.item("Previous", "Flip a page.", Material.BLACK_STAINED_GLASS_PANE));
+				inv.setItem(49, ItemModule.item("Back", "Go back a page.", Material.BLACK_STAINED_GLASS_PANE));
+				inv.setItem(53, ItemModule.item("Next", "Flip a page.", Material.BLACK_STAINED_GLASS_PANE));
+				handNavigation.add(inv);
+			}
+			inv.setItem(i % breakEach, handRecipeItems.get(i));
+		}
+	}
+	
+	public static void translateHandRecipe(ItemStack result, ItemStack[] required, String shape, String ids) {
+		Inventory recipe = Bukkit.createInventory(null, 36, "Hand Recipe Guide");
+		String[] the_ids = ids.split(",");
+		for (int i = 0; i < the_ids.length; i++) {
+			for (int n = 0; n < shape.length(); n++) {
+				char c = shape.charAt(n);
+				if (c == the_ids[i].charAt(0)) {
+					if (n > 1) {
+						recipe.setItem(n + 7, required[i]);
+					} else {
+						recipe.setItem(n, required[i]);
+					}
+				}
+			}
+		}
+		recipe.setItem(2, ItemModule.item("--->", "--->", Material.BLACK_STAINED_GLASS_PANE));
+		recipe.setItem(3, result);
+		recipe.setItem(18 + 9, ItemModule.item("Previous", "Flip a page.", Material.BLACK_STAINED_GLASS_PANE));
+		recipe.setItem(18 + 9 + 4, ItemModule.item("Back", "Go back a page.", Material.BLACK_STAINED_GLASS_PANE));
+		recipe.setItem(26 + 9, ItemModule.item("Next", "Flip a page.", Material.BLACK_STAINED_GLASS_PANE));
+		translatedHandRecipes.add(recipe);
+	}
+	
 	public static void craftedRecipe(String key, ItemStack item, String rows, ItemStack[] ingredients, String ids) {
 		NamespacedKey namespacedKey = new NamespacedKey(ValkUtils.plugin, key);
 		ShapedRecipe recipe = new ShapedRecipe(namespacedKey, item);
@@ -47,6 +92,8 @@ public class ItemModule {
 		}
 		ValkUtils.plugin.getServer().addRecipe(recipe);
 		ValkUtils.plugin.recipes.add(new NamespacedKey(ValkUtils.plugin, key));
+		translateHandRecipe(item, ingredients, rows, ids);
+		handRecipeItems.add(item);
 	}
 	
 	/*
@@ -79,6 +126,7 @@ public class ItemModule {
 		}
 		im.setLore(list);
 		item.setItemMeta(im);
+		VinkaItems.items.add(item);
 		return item;
 	}
 	
@@ -93,6 +141,7 @@ public class ItemModule {
 		}
 		im.setLore(list);
 		item.setItemMeta(im);
+		VinkaItems.items.add(item);
 		return item;
 	}
 }
--- src/com/valkutils/modules/MobModule.java
+++ src/com/valkutils/modules/MobModule.java
@@ -1,11 +1,22 @@
 package com.valkutils.modules;
 
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 
 public class MobModule {
-	public static int countMobs(World w, EntityType type) {
+	public static int countMobsInChunk(Entity[] entities, EntityType type) {
+		int count = 0;
+		for (Entity entity : entities) {
+			if (entity.getType() == type) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static int countMobsInWorld(World w, EntityType type) {
 		int count = 0;
 		for (LivingEntity entity : w.getLivingEntities()) {
 			if (entity.getType() == type) {
